e485c1b183e34ef089b840901935adbee15c10ce
mzaki23 <mzakiatlas@gmail.com>
2022-03-16 08:26:45 +0700

    master

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/{id:notelp}', [Controller::class, 'show']);
-Route::get('/', [Controller::class, 'index']);
 
 // Route::get('/', function () {
 //     return view('test');
